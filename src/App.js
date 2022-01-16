857034e1e1f143c489caabadb275eb089ad953ba
Albert Xie <albert.xie27@gmail.com>
2022-01-16 17:37:30 -0500

    content wip

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Still working on it!</h1>
+        <h1 className='App-h1'>Hello!</h1>
+        <table>
+          <tr>
+            <th><a className='App-link' href="https://www.linkedin.com/in/albertxie/">Linkedin</a></th>
+            <th><a className='App-link' href="https://github.com/albertxie">Github</a></th>
+            <th><a className='App-link' href="https://twitter.com/albertxiee">Twitter</a></th>
+          </tr>
+        </table>
       </header>
     </div>
   );
