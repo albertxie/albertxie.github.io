05c8826c442480c58357c4cfdc0555cd50aa64b8
Albert Xie <albert.xie27@gmail.com>
2022-01-23 17:49:51 -0500

    content

--- src/App.js
+++ src/App.js
@@ -4,12 +4,15 @@ function App() {
   // Homepage
   return (
     <div className='content'>
-      <h1 className='title'>Hello World</h1>
-      <p className='subtitle'>Welcome to my site</p>
-      <p> Lorem ipsum dolor sit amjet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vitae ultricies leo integer malesuada nunc vel risus. Nisi est sit amet facilisis. Enim diam vulputate ut pharetra sit amet aliquam. Arcu odio ut sem nulla pharetra diam sit amet nisl. Eu mi bibendum neque egestas congue quisque egestas diam. Dignissim suspendisse in est ante in nibh mauris cursus mattis. Laoreet suspendisse interdum consectetur libero id faucibus. Pulvinar neque laoreet suspendisse interdum consectetur libero. Eu consequat ac felis donec. Ac turpis egestas maecenas pharetra convallis posuere morbi leo urna. Venenatis a condimentum vitae sapien pellentesque habitant morbi tristique senectus. Id cursus metus aliquam eleifend mi. Vitae nunc sed velit dignissim sodales ut eu. Dictum non consectetur a erat nam at lectus urna duis. Consequat mauris nunc congue nisi vitae suscipit tellus.</p>
+      <h1 className='title'>Hello!</h1>
+      <p>My name is Albert, I am a sofware engineer at <a href='#'>Instacart</a>, welcome to my page! </p>
       <a href='/about'>about</a>
       <br></br>
       <a href='/blog'>blog</a>
+      <br></br>
+      <a href='https://www.github.com/albertxie'>github</a>
+      <br></br>
+      <a href='https://www.linkedin.com/in/albertxie/'>linkedin</a>
     </div>
   );
 }
