2b4bb6068cd326b5082bb6be53acff56f729b00f
Albert Xie <albert.xie27@gmail.com>
2022-01-22 22:14:56 -0500

    wip

--- src/App.js
+++ src/App.js
@@ -4,12 +4,10 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1 className='App-h1'>Hello!</h1>
+        <h1 className='App-h1'>Home</h1>
         <table>
           <tr>
-            <th><a className='App-link' href="https://www.linkedin.com/in/albertxie/">Linkedin</a></th>
-            <th><a className='App-link' href="https://github.com/albertxie">Github</a></th>
-            <th><a className='App-link' href="https://twitter.com/albertxiee">Twitter</a></th>
+            <th><a className='App-link' href="/about">about</a></th>
           </tr>
         </table>
       </header>
